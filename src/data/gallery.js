9e17c00903c8458246170848692375780489335b
ZenArtZ98 <ZenArtZ98@users.noreply.github.com>
2026-09-19 16:25:28 +0500

    Update gallery photos and category icons

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,6 +1,6 @@
 import { asset } from '../utils/asset';
 
-export const GALLERY_PHOTOS = [
+const GALLERY_PHOTOS_UNSHUFFLED = [
   {
     bg: asset('assets/images/md-11.webp'),
     w: 'min(70vw,280px)',
@@ -8,7 +8,31 @@ export const GALLERY_PHOTOS = [
     rot: '-2deg',
   },
   {
-    bg: asset('assets/images/shoot2-m2-15.webp'),
+    bg: asset('assets/images/gallery-01.webp'),
+    w: 'min(70vw,260px)',
+    h: '390px',
+    rot: '-3deg',
+  },
+  {
+    bg: asset('assets/images/md-05.webp'),
+    w: 'min(70vw,300px)',
+    h: '330px',
+    rot: '3deg',
+  },
+  {
+    bg: asset('assets/images/gallery-03.webp'),
+    w: 'min(70vw,250px)',
+    h: '360px',
+    rot: '-1deg',
+  },
+  {
+    bg: asset('assets/images/md-09.webp'),
+    w: 'min(70vw,280px)',
+    h: '360px',
+    rot: '1deg',
+  },
+  {
+    bg: asset('assets/images/gallery-02.webp'),
     w: 'min(70vw,270px)',
     h: '400px',
     rot: '2deg',
@@ -20,39 +44,69 @@ export const GALLERY_PHOTOS = [
     rot: '-1deg',
   },
   {
-    bg: asset('assets/images/md-05.webp'),
-    w: 'min(70vw,300px)',
-    h: '330px',
+    bg: asset('assets/images/gallery-04.webp'),
+    w: 'min(70vw,290px)',
+    h: '390px',
     rot: '3deg',
   },
   {
-    bg: asset('assets/images/shoot2-m2-06.webp'),
+    bg: asset('assets/images/gallery-05.webp'),
     w: 'min(70vw,260px)',
+    h: '400px',
+    rot: '-2deg',
+  },
+  {
+    bg: asset('assets/images/shoot2-m2-15.webp'),
+    w: 'min(70vw,270px)',
+    h: '400px',
+    rot: '2deg',
+  },
+  {
+    bg: asset('assets/images/gallery-06.webp'),
+    w: 'min(70vw,300px)',
+    h: '330px',
+    rot: '2deg',
+  },
+  {
+    bg: asset('assets/images/gallery-07.webp'),
+    w: 'min(70vw,270px)',
     h: '390px',
     rot: '-3deg',
   },
   {
-    bg: asset('assets/images/md-09.webp'),
-    w: 'min(70vw,280px)',
+    bg: asset('assets/images/gallery-08.webp'),
+    w: 'min(70vw,300px)',
     h: '360px',
     rot: '1deg',
   },
   {
-    bg: asset('assets/images/shoot2-m2-21.webp'),
-    w: 'min(70vw,260px)',
-    h: '390px',
+    bg: asset('assets/images/gallery-09.webp'),
+    w: 'min(70vw,290px)',
+    h: '380px',
+    rot: '-2deg',
+  },
+  {
+    bg: asset('assets/images/gallery-10.webp'),
+    w: 'min(70vw,270px)',
+    h: '400px',
     rot: '2deg',
   },
-  {
-    bg: asset('assets/images/md-13.webp'),
-    w: 'min(70vw,275px)',
-    h: '370px',
-    rot: '-1deg',
-  },
-  {
-    bg: asset('assets/images/shoot2-m2-03.webp'),
-    w: 'min(70vw,255px)',
-    h: '400px',
-    rot: '3deg',
-  },
+];
+
+export const GALLERY_PHOTOS = [
+  GALLERY_PHOTOS_UNSHUFFLED[11],
+  GALLERY_PHOTOS_UNSHUFFLED[2],
+  GALLERY_PHOTOS_UNSHUFFLED[5],
+  GALLERY_PHOTOS_UNSHUFFLED[9],
+  GALLERY_PHOTOS_UNSHUFFLED[14],
+  GALLERY_PHOTOS_UNSHUFFLED[0],
+  GALLERY_PHOTOS_UNSHUFFLED[7],
+  GALLERY_PHOTOS_UNSHUFFLED[4],
+  GALLERY_PHOTOS_UNSHUFFLED[12],
+  GALLERY_PHOTOS_UNSHUFFLED[6],
+  GALLERY_PHOTOS_UNSHUFFLED[1],
+  GALLERY_PHOTOS_UNSHUFFLED[10],
+  GALLERY_PHOTOS_UNSHUFFLED[3],
+  GALLERY_PHOTOS_UNSHUFFLED[13],
+  GALLERY_PHOTOS_UNSHUFFLED[8],
 ];
